81ee802c22d873c23010968460378fb66a9ad7cf
Scanu46 <nykb24@gmail.com>
2022-11-17 06:37:28 +0000

    Topnav toggle responsive

---
+++ Assets/Java/Topnav.js
@@ -0,0 +1,9 @@
+<!--Toggle "responsive" class when icon is clicked-->
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
